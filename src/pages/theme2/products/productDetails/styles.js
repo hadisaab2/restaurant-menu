d01bc908a4826fc66c4b0c260c6f4a1b8fc91249
Hadi Saab <hadi.saab@montyholding.com>
2024-04-06 21:24:05 +0300

    fix refs

--- src/pages/theme2/products/productDetails/styles.js
+++ src/pages/theme2/products/productDetails/styles.js
@@ -8,7 +8,7 @@ const slideAnimation = (x, y,width) => keyframes`
     left: ${x}px;
     top:${y}px;
     width:${width}px;
-    min-height:20vh;
+    height:20vh;
     border-radius: 10px;
     
 }
@@ -16,12 +16,64 @@ const slideAnimation = (x, y,width) => keyframes`
     left: 0;
     top:0;
     width:100%;
-    min-height: 100vh;
+    height: 100vh;
     border-radius: 0px;
 
 }
 `;
 
+export const Wrapper = styled.div`
+position:fixed;
+  width: ${props=>props.CloseAnimation?"100%":`${props.width}px`};
+  top:${props=>props.CloseAnimation?"0":`${props.y}px`};
+left:${props=>props.CloseAnimation?"0":`${props.x}px`};
+  height: ${props=>props.CloseAnimation?"100vh":`20vh`};
+  overflow: hidden;
+  transition:all 0.8s ;
+  animation: ${({ x, y,width }) => slideAnimation(x, y,width)} 0.8s;
+  z-index: 6;
+  border-radius: 0px;
+  background-color: ${props=>props.theme.backgroundColor};
+  display: flex;
+  flex-direction:column;
+  align-items:center; 
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+
+const ImageAnimation  = keyframes`
+ 0% { 
+    width:100%;
+    height:20vh;
+    border-radius: 10px;
+    top:0px;
+}
+
+ 100% { 
+    width:90%;
+    height:45vh;
+    border-radius: 40px;
+    top:80px;
+
+    }
+`;
+export const ImageContainer = styled.div`
+  width: ${props=>props.CloseAnimation?"90%":"100%"};
+height:${props=>props.CloseAnimation?"45vh":"20vh"}; 
+position: absolute;
+border-radius: ${props=>props.CloseAnimation?"40px":"10px"};
+top:${props=>props.CloseAnimation?"80px":"0px"};
+overflow: hidden;
+transition:all 0.8s;
+animation:${ImageAnimation} 0.8s;
+box-shadow: 0px -4px 8px rgba(0, 0, 0, 0.1); 
+
+`
 const BackIconAnimation  = keyframes`
  0% { 
     left:-90px;
@@ -88,62 +140,13 @@ export const Category = styled.span`
 
 
 `;
-export const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
 
-
-export const Wrapper = styled.div`
-position:fixed;
-  width: ${props=>props.CloseAnimation?"100%":`${props.width}px`};
-  top:${props=>props.CloseAnimation?"0":`${props.y}px`};
-left:${props=>props.CloseAnimation?"0":`${props.x}px`};
-  min-height: ${props=>props.CloseAnimation?"100vh":`20vh`};
-  overflow: scroll;
-  transition:all 0.8s ease-in-out;
-  animation: ${({ x, y,width }) => slideAnimation(x, y,width)} 0.8s ease-in-out;
-  z-index: 6;
-  border-radius: 0px;
-  background-color: ${props=>props.theme.backgroundColor};
-  display: flex;
-  flex-direction:column;
-  align-items:center; 
-`;
-const ImageAnimation  = keyframes`
- 0% { 
-    width:100%;
-    height:20vh;
-    border-radius: 10px;
-    top:0px;
-}
-
- 100% { 
-    width:90%;
-    height:45vh;
-    border-radius: 40px;
-    top:80px;
-
-    }
-`;
-export const ImageContainer = styled.div`
-  width: ${props=>props.CloseAnimation?"90%":"100%"};
-height:${props=>props.CloseAnimation?"45vh":"20vh"}; 
-position: absolute;
-border-radius: ${props=>props.CloseAnimation?"40px":"10px"};
-top:${props=>props.CloseAnimation?"80px":"0px"};
-overflow: hidden;
-transition:all 0.8s;
-animation:${ImageAnimation} 0.8s;
-box-shadow: 0px -4px 8px rgba(0, 0, 0, 0.1); 
-
-`
 export const FakeContainer = styled.div`
   width: ${props=>props.CloseAnimation?"90%":"100%"};
   height:${props=>props.CloseAnimation?"45vh":"20vh"}; 
 border-radius: ${props=>props.CloseAnimation?"40px":"10px"};
 margin-top:${props=>props.CloseAnimation?"80px":"0px"};
+display:${props=>props.CloseAnimation?"flex":"none"};
 overflow: hidden;
 transition:all 1s;
 animation:${ImageAnimation} 0.8s;
